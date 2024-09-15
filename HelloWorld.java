cceab538ff36d554a66d1e98acdae23b7704f321
RayChaun <rraglin0@lab.cpcc.edu>
2024-09-14 20:49:14 -0400

    Added new print statement Ln6
    Fixed code and added a new print statement on line 6.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,9 @@
-Public class HelloWorld
+public class HelloWorld
 {
-   Public static void main (String[] args)
+   public static void main (String[] args)
    {
-      System.out.print("Hello world!");
+      System.out.println("Hello world!");
+      System.out.println("Program modified, but able to revert to original version using Github Desktop.");
    }
 }
       
